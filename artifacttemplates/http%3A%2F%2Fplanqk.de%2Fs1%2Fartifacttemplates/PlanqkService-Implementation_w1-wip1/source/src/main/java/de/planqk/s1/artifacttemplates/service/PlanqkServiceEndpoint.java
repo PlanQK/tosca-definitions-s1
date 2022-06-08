60c08fb6effdeee95c6c4a67f96a29150378c887
Michael Wurster <miwurster@googlemail.com>
2022-06-08 15:57:25 +0200

    add initial code to get access token

--- artifacttemplates/http%3A%2F%2Fplanqk.de%2Fs1%2Fartifacttemplates/PlanqkService-Implementation_w1-wip1/source/src/main/java/de/planqk/s1/artifacttemplates/service/PlanqkServiceEndpoint.java
+++ artifacttemplates/http%3A%2F%2Fplanqk.de%2Fs1%2Fartifacttemplates/PlanqkService-Implementation_w1-wip1/source/src/main/java/de/planqk/s1/artifacttemplates/service/PlanqkServiceEndpoint.java
@@ -3,6 +3,11 @@ package de.planqk.s1.artifacttemplates.service;
 import de.planqk.s1.artifacttemplates.OpenToscaHeaders;
 import de.planqk.s1.artifacttemplates.SoapUtil;
 import java.util.Objects;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ws.context.MessageContext;
@@ -31,6 +36,30 @@ public class PlanqkServiceEndpoint {
 
             SoapUtil.sendSoapResponse(invokeResponse, InvokeResponse.class, openToscaHeaders.replyTo());
             return;
+        }
+
+        var deploymentArtifact = openToscaHeaders.deploymentArtifacts()
+            .entrySet()
+            .stream()
+            .flatMap(e -> e.getValue().entrySet().stream())
+            .findFirst()
+            .orElseThrow();
+
+        var url = deploymentArtifact.getValue();
+        var file = FileHandler.downloadFile(url);
+
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+            String authUrlTemplate = "%s/auth/realms/planqk/protocol/openid-connect/token?grant_type=password&username=%s&password=%s";
+            String authUrl = authUrlTemplate.formatted(request.getBaseUrl(), request.getUsername(), request.getPassword());
+            var response = client.execute(new HttpGet(authUrl));
+
+            HttpEntity entity = response.getEntity();
+            String responseString = EntityUtils.toString(entity, "UTF-8");
+
+            // TODO: move to separate method
+
+        } catch (Exception e) {
+            LOG.error("Error requesting PlanQK Platform access token");
         }
 
         SoapUtil.sendSoapResponse(invokeResponse, InvokeResponse.class, openToscaHeaders.replyTo());
